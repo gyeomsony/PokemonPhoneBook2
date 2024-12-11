4a041bd0211733d197db4e90258158a8c5fa53af
sonny <gyeomsony@gmail.com>
2024-12-11 14:49:44 +0900

    ♻️ Alamofire 사용하여 리팩토링

--- PokemonPhoneBook2/PokemonPhoneBook2/Model/PokemonAPI.swift
+++ PokemonPhoneBook2/PokemonPhoneBook2/Model/PokemonAPI.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 // 포켓몬 데이터 모델
 struct PokemonResult: Codable {
@@ -24,64 +25,34 @@ struct Sprites: Codable {
     }
 }
 
-// 제네릭으로 데이터를 가져오는 fetchData 함수
-func fetchData<T: Decodable>(url: URL, completion: @escaping (T?) -> Void) {
-    let session = URLSession(configuration: .default)
-    session.dataTask(with: URLRequest(url: url)) { data, response, error in
-        guard let data = data, error == nil else {
-            print("데이터 로드 실패")
-            completion(nil)
-            return
-        }
-        
-        let successRange = 200..<300
-        if let response = response as? HTTPURLResponse, successRange.contains(response.statusCode) {
-            do {
-                let decodedData = try JSONDecoder().decode(T.self, from: data)
-                completion(decodedData)
-            } catch {
-                print("JSON 디코딩 실패: \(error)")
-                completion(nil)
-            }
-        } else {
-            completion(nil)
-        }
-    }.resume()
-}
-
 // 포켓몬 이미지 데이터를 가져오는 함수
 func fetchRandomPokemonImage(completion: @escaping (UIImage?) -> Void) {
     let randomID = Int.random(in: 1...1000)
     let urlString = "https://pokeapi.co/api/v2/pokemon/\(randomID)"
     
-    guard let url = URL(string: urlString) else {
-        print("잘못된 URL")
-        completion(nil)
-        return
-    }
-    
-    fetchData(url: url) { (pokemonResponse: PokemonResult?) in
-        // pokemonResponse가 nil인 경우 리턴
-        guard let response = pokemonResponse else {
-            completion(nil)
-            return
-        }
-        
-        guard let imageUrl = URL(string: response.sprites.frontDefault) else {
-            completion(nil)
-            return
-        }
-        
-        DispatchQueue.global().async {
-            if let data = try? Data(contentsOf: imageUrl), let image = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    completion(image)
-                }
-            } else {
-                DispatchQueue.main.async {
-                    completion(nil)
+    AF.request(urlString).responseDecodable(of: PokemonResult.self) { response in
+        switch response.result {
+        case .success(let pokemonResponse):
+            guard let imageUrl = URL(string: pokemonResponse.sprites.frontDefault) else {
+                completion(nil)
+                return
+            }
+            
+            DispatchQueue.global().async {
+                if let data = try? Data(contentsOf: imageUrl), let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        completion(image)
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        completion(nil)
+                    }
                 }
             }
+        case .failure(let error):
+            print("Request failed with error: \(error.localizedDescription)")
+            completion(nil)
         }
     }
 }
+    
